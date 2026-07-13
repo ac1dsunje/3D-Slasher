d2222366e641aac976531e87b51a9f04b7fd8196
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-13 14:38:48 +0200

    added tags and chunk controller

---
+++ Assets/_Game/Scripts/Chunks/ChunkController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Game.Scripts.Chunks
+{
+public class ChunkController: MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player collided with " + collision.gameObject.name);
+        }
+    }
+}
+}
